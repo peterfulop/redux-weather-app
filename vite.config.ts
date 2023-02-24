7bb43964ff19b1ff1a6d030a4446cac8651aadf7
peterfulop <mrpeterfulop@gmail.com>
2023-02-24 11:42:33 +0100

    feat(*): HashRouter;

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/redux-weather-app/',
+  base: '/redux-weather-app/',
 });
