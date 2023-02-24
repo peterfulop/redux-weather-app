4b0a03b2bcac610e16e658481161ebe221b588b5
peterfulop <mrpeterfulop@gmail.com>
2023-02-24 11:30:39 +0100

    feat(*): HashRouter;

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { store } from './state';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
